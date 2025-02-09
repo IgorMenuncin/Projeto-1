3acd9f9f6026ef8f2f88a7564499476d654b7420
Igor <igormenuncin@gmail.com>
2025-02-09 15:45:36 -0300

    Primeiro commit

---
+++ Backend/src/test/java/projeto1com/example/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto1com.example.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
