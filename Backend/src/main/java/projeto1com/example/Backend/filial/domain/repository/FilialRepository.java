269dae2b012228096cf4fa426a94af2c6aa01900
Igor <igormenuncin@gmail.com>
2025-05-19 13:29:29 -0300

    Subindo restante

--- Backend/src/main/java/projeto1com/example/Backend/filial/domain/repository/FilialRepository.java
+++ Backend/src/main/java/projeto1com/example/Backend/filial/domain/repository/FilialRepository.java
@@ -3,7 +3,11 @@ package projeto1com.example.Backend.filial.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import projeto1com.example.Backend.filial.domain.model.Filial;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FilialRepository extends JpaRepository<Filial, UUID> {
+
+    Optional<Filial> findByNome(String nome);
+
 }
---
+++ Backend/src/test/java/projeto1com/example/Backend/filial/domain/repository/FilialRepositoryTest.java
@@ -0,0 +1,15 @@
+package projeto1com.example.Backend.filial.domain.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+@ActiveProfiles("test")
+class FilialRepositoryTest {
+
+    @Test
+    void findByNome() {
+    }
+}
